e6a6ce5ba975d26e8c253d50fd6557821586804e
polyacetal <yotyan988@gmail.com>
2023-01-11 11:40:42 +0900

    move man

--- 1215/MoveFrame.java
+++ 1215/MoveFrame.java
@@ -1,9 +1,12 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.event.*;
 
-public class MoveFrame extends JFrame
+public class MoveFrame extends JFrame implements ActionListener
 {
     private MovePanel mp;
+    private Timer timer;
 
     public MoveFrame()
     {
@@ -11,18 +14,26 @@ public class MoveFrame extends JFrame
         Image[] chara_array = new Image[3];
         for (int i = 0; i < 3; i++)
         {
-            chara_array[i] = tk.getImage("chara" + i + ".jpg");
+            chara_array[i] = tk.getImage("chara_move" + i + ".jpg");
         }
         this.mp = new MovePanel(chara_array);
         JPanel panel = new JPanel();
         panel.add(this.mp);
         super.getContentPane().add(panel);
+        this.timer = new Timer(17, this);
+        this.timer.start();
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+        this.mp.repaint();
+        Toolkit.getDefaultToolkit().sync();
     }
 
     public static void main(String[] args)
     {
         MoveFrame a = new MoveFrame();
-        a.setSize(640, 480);
+        a.setSize(640, 500);
         a.setLocation(100,100);
         a.setVisible(true);
     }
--- 1215/MovePanel.java
+++ 1215/MovePanel.java
@@ -4,21 +4,35 @@ import javax.swing.*;
 public class MovePanel extends JPanel
 {
     private Image[] chara_array;
+    private int chara_i, chara_x, chara_y, delay;
 
     public MovePanel(Image[] chara_array)
     {
         this.chara_array = chara_array;
         super.setBackground(Color.white);
         super.setPreferredSize(new Dimension(640,480));
+        this.chara_i = 0;
+        this.chara_x = 250;
+        this.chara_y = 0;
+        this.delay = 0;
     }
 
     public void paintComponent(Graphics g)
     {
         System.out.println("in paintComponent:");
         super.paintComponent(g);
-        for (int i = 0; i < this.chara_array.length; i++)
+        this.delay++;
+        if(this.delay > 5)
         {
-            g.drawImage(this.chara_array[i], i * 150 + 120, 150, this);
+        this.delay = 0;
+        this.chara_i++;
+        this.chara_i = this.chara_i % this.chara_array.length;
         }
+        this.chara_x -=2;
+        if(this.chara_x < 0)
+        {
+            this.chara_x = 250;
+        }
+        g.drawImage(this.chara_array[this.chara_i], this.chara_x, this.chara_y,this);
     }
 }
